3c82eef5a559f1096372bd793eabc0ba5d549cff
gasdokumenter <gasdokumenter@gmail.com>
2026-03-14 11:36:52 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'gas-dok-v1';
+const assets = ['./index.html', './manifest.json'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
